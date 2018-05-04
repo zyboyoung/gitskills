6b81ad1bc033c5666862dd6d8e060149a963422f
pigfish <627644532@qq.com>
2018-05-05 00:12:54 +0800

    complete one_to_some

--- ChinaVis/StaffStructure/one_to_some.py
+++ ChinaVis/StaffStructure/one_to_some.py
@@ -1,11 +1,14 @@
 import pandas as pd 
 import os
+import re
 
 # 得到30个文件的名字
 root_path = '../raw_data'
 file_path = []
 for root, dirs, files in os.walk(root_path):
     file_path.append(root)
+
+file_path.remove(root_path)
 
 def one_to_some(file_name):
     """
@@ -17,20 +20,49 @@ def one_to_some(file_name):
     for index, row in df.iterrows():
         to_list = row['to'].split(';')
         if len(to_list) > 1:
-            df_new = pd.concat([df_new, df[df.index==index]])
-    
+            df_new = pd.concat([df_new, df[df.index==index]], ignore_index=True)
+
+    df_new = df_new.reset_index(drop=True)
     return df_new
 
 
+def print_dict(df):
+    df_new = pd.DataFrame(columns=['from', 'to'])
+    pattern = re.compile(r'(\d{4})@hightech\.com')
+    i = 0
+
+    for _, row in df.iterrows():
+        id_from = re.search(pattern, row['from']).group(1)
+        id_to = re.findall(pattern, row['to'])
+        df_new.loc[i] = {'from': id_from, 'to': tuple(id_to)}
+        i += 1
+
+    df_new = df_new.drop_duplicates()
+
+    return df_new
+
 if __name__=='__main__':
-    id_class = 'dev'
+    id_class = 'hr'
     df = pd.DataFrame()
 
     for file in file_path:
-        file_name = file+'/email_'+id_class+'.csv'
+        file_name = file+'\email_'+id_class+'.csv'
         df = pd.concat([df, one_to_some(file_name)])
-    
-    df.to_csv(id_class+'_one_to_some.csv')
+
+    try:
+        df.to_csv('one_to_some\\'+id_class+'_one_to_some.csv', encoding='gbk')
+        print('已生成'+id_class+'_one_to_some文件')
+    except:
+        print(df)
+        print('该文件已经存在')
+
+    df_new = print_dict(df)
+    try:
+        df_new.to_csv('one_to_some\id_'+id_class+'_one_to_some.csv')
+        print('已生成id_'+id_class+'_one_to_some文件')
+    except:
+        print(df_new)
+        print('该文件已经存在')
 
 
 
